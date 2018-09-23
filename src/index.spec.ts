97d5acf835b7e1826041f8ff15147e4a04f6d2f2
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-09-23 19:28:13 -0400

    Verifying unit tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,7 @@
-it("logs hello world", async () => {
+it('logs hello world', async () => {
   let loggedMessage: string;
   global.console.log = message => (loggedMessage = message);
-  await import("./index");
-  expect(loggedMessage!).toEqual("hello world!");
+
+  await import('./index');
+  expect(loggedMessage!).toEqual('hello world!');
 });
