12b07f1743120c4ce45f8a3e2b2c3cd3090911bf
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-09-24 00:41:56 -0400

    Add CORS

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
+import cors from 'cors';
 import express, { json } from 'express';
 import helmet from 'helmet';
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: [
+      'http://localhost:8080', // front-end development URL
+      'https://onlinecheckersplatform.com',
+    ],
+  }),
+);
 app.use(helmet());
 app.use(json());
 
 app.all('*', (req, res) => res.send('Hello, world'));
-
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
