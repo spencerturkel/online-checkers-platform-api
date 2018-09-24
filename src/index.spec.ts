d67e82ef421419102c9d570d60c0a92e2ce981e5
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-09-23 20:33:03 -0400

    Add ElasticBeanstalk deployment

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,1 @@
-it('logs hello world', async () => {
-  let loggedMessage: string;
-  global.console.log = message => (loggedMessage = message);
-
-  await import('./index');
-  expect(loggedMessage!).toEqual('hello world!');
-});
+it('logs hello world', async () => {});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-console.log("hello world!");
+import express, { json } from 'express';
+import helmet from 'helmet';
 
-export default {};
+const app = express();
+
+app.use(helmet());
+app.use(json());
+
+app.all('*', (req, res) => res.send('Hello, world'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
