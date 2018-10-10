b54af49e15b203b6c0cff59603df4c543a0c1300
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-10-10 15:16:21 -0400

    Writing waiting

--- src/server.ts
+++ src/server.ts
@@ -136,10 +136,23 @@ server.post('/start-game', authenticate, (req, res) => {
     waitingUser = req.session!.userID;
   } else {
     const playerOneId = waitingUser;
-    const playerTwoId = req.session!.id; // TODO: handle same user as waiting
+    const playerTwoId = req.session!.userId; // TODO: handle same user as waiting
     const letter = 'A'; // TODO: make this a random letter
 
-    games[playerOneId + playerTwoId] = { playerOneId, playerTwoId, letter };
+    games[playerOneId + playerTwoId] = { playerOneId, playerTwoId, letter }; // TODO: better scheme for keying games
+  }
+});
+
+server.get('/waiting', authenticate, (req, res) => {
+  const userId = req.session!.userId;
+  const gamesWithPlayer = Object.values(games).filter(
+    x => x.playerOneId === userId || x.playerTwoId === userId,
+  );
+
+  if (gamesWithPlayer.length === 0) {
+    res.sendStatus(204);
+  } else {
+    res.send({ game: gamesWithPlayer }); // TODO don't send all the fields
   }
 });
 
