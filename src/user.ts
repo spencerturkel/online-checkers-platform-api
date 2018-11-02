26bdfa386ebe25e6dffc800125a401ffa4ad116b
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-11-01 23:25:34 -0400

    Connect user endpoint to dynamo

--- src/user.ts
+++ src/user.ts
@@ -1,5 +1,6 @@
 import { Request, Router } from 'express';
 import { authenticate } from './auth/middleware';
+import { documents, tableName } from './dynamo';
 
 export interface User {
   gameId?: string;
@@ -16,44 +17,11 @@ export const userRouter = Router();
 
 userRouter.use(authenticate);
 
-declare module 'express' {
-  interface Request {
-    user?: User;
-  }
-}
+userRouter.get('/', async (req: Request, res) => {
+  const { userId } = req.session!;
 
-userRouter.use((req: Request, res, next) => {
-  const currentUserId = req.session!.userId;
-
-  req.user = users.get(currentUserId);
-
-  if (req.user) {
-    next();
-    return;
-  }
-
-  req.session!.destroy(err => {
-    if (err) {
-      next(err);
-    }
-
-    res.sendStatus(500);
-  });
-});
-
-userRouter.get('/info', (req: Request, res) => {
-  const user = req.user!;
-
-  res.json({ gameId: user.gameId, name: user.name });
-});
-
-userRouter.get('/stats', (req: Request, res) => {
-  const user = req.user!;
-
-  res.json({
-    name: user.name,
-    wins: user.wins,
-    losses: user.losses,
-    games: user.wins + user.losses,
-  });
+  res.json(
+    (await documents.get({ Key: { userId }, TableName: tableName }).promise())
+      .Item,
+  );
 });
